9d45e6038f636fdcd5255df525175530607e8477
Zo Zhang <ZoZhang@users.noreply.github.com>
2019-01-28 17:26:30 +0100

    Initialise a script

---
+++ zorecom.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# a simple remote command execution script
+# 
+# @author Zhao ZHANG <zo.zhang@gmail.com>
+# Usage: myscript
+#
+# Options:
+#   -h, --help    Display help message.
+#
